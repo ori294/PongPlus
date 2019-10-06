3a8d35e6bb84b19d147efb3973afce2104605533
asafam <13185899+asafam@users.noreply.github.com>
2019-10-06 17:15:54 +0300

    Game controller object

---
+++ Assets/Scripts/PlayerBorder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBorder : MonoBehaviour
+{
+    public IPlayerBorderListener listener;
+    // public ePlayer player;
+    // public ScoreUI score;
+    public ePlayer player;
+    
+    public AudioSource sfx;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sfx = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        Ball ball = collider.gameObject.GetComponent<Ball>();
+        if (ball != null) {
+            sfx.Play();
+            listener.OnPlayerBorderCollisionEnter(player, ball);
+            // ball.transform.position = new Vector3(0f, 1f, 0f);
+
+            // if (player == ePlayer.Right) score.scorePlayerRed++;
+            // else if (player == ePlayer.Left) score.scorePlayerBlue++;
+            
+        }
+    }
+}
+
+public interface IPlayerBorderListener {
+    void OnPlayerBorderCollisionEnter(ePlayer player, Ball ball);
+}
---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUI : MonoBehaviour
+{
+    public int scorePlayerRed;
+    public int scorePlayerBlue;
+    public GUIStyle style;
+
+    void OnGUI()
+    {
+        float x = Screen.width / 2f;
+        float y = 30f;
+        float width = 300f;
+        float height = 20f;
+        string text = scorePlayerRed + " / " + scorePlayerBlue;
+
+        GUI.Label(new Rect(x-(width/2f), y, width, height), text, style);
+    }
+}
