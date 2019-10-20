5a9b2ebe45062f6d0802622f2689100dde67d978
Ori Levy <ori294@gmail.com>
2019-10-20 11:22:13 +0300

    Fixed back button bug, Added Bouncers to Single
    Back buttons now work in the winner screen.
    Added bouncers to the single player (instead of the axis inversion). These are pink blocks that moves across the Z axis from time to time (You can determine their first move, but it will be randomly generated from time to time during the game, causing their movement to be  unexpectable)

---
+++ Assets/Scripts/Bouncer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    public Vector3 pointA = new Vector3(0, 0, 0);
+    public Vector3 pointB = new Vector3(0, 0, 0);
+    public float len = 10f;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time / 2, len));
+
+        if (Time.time % (2 * len) == 0) 
+        {
+            len = Random.Range(2f, 10f);
+        }
+    }
+}
